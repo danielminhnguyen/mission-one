91ec2a65686d76fd2cbffab2d6d9d2d75820516b
hokkinxx <minhngyen68@hotmail.com>
2020-10-06 13:50:50 +1300

    first commit

---
+++ profile.js
@@ -0,0 +1,39 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navigation");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    var scrollPos = window.pageYOffset
+  if (scrollPos >= sticky) {
+    navbar.classList.add("sticky")
+    if (scrollPos >= top1 && scrollPos < top2) {
+        document.getElementById("about").classList.add
+    }
+  } else {
+    navbar.classList.remove("sticky");
+  }
+  
+}
+
+
+
+var top1 = $('#home').offset().top;
+var top2 = $('#about').offset().top;
+var top3 = $('#portfolio').offset().top;
+
+$(document).scroll(function() {
+  var scrollPos = $(document).scrollTop();
+  if (scrollPos >= top1 && scrollPos < top2) {
+    $('#change').css('background-color', '#f00');
+  } else if (scrollPos >= top2 && scrollPos < top3) {
+    $('#change').css('background-color', '#0f0');
+  } else if (scrollPos >= top3) {
+    $('#change').css('background-color', '#00f');
+  }
+});

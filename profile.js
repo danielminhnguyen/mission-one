5716e433cd9823eedd1a7e6f9951fe8bb104c138
hokkinxx <minhngyen68@hotmail.com>
2020-10-08 19:16:41 +1300

    add mobile menu

--- profile.js
+++ profile.js
@@ -14,9 +14,9 @@ function StickyNavBar() {
     var scrollPos = window.pageYOffset
   if (scrollPos >= sticky) {
     navbar.classList.add("sticky")
-    if (scrollPos >= top1 && scrollPos < top2) {
-        document.getElementById("about").classList.add
-    }
+    // if (scrollPos >= top1 && scrollPos < top2) {
+    //     document.getElementById("about").classList.add
+    // }
   } else {
     navbar.classList.remove("sticky");
   }
@@ -25,28 +25,40 @@ function StickyNavBar() {
 
 
 // Mobile Menu
+// open mobile menu $ : document.getElementById()
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function menuToggle() {
+   document.getElementById('nav-wrap').classList.toggle("show");
+   document.getElementById('navigation').classList.toggle("open");
+}
 
-function myFunction() {
-  var x = document.getElementById("navigation");
-  if (x.className === "topnav") {
-  x.className += " responsive"; 
-  } else {
-    x.className = "topnav";
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('js-toggle-menu')) {
+    var dropdowns = document.getElementById('nav-wrap');
+   
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        
+        openDropdown.classList.remove('show');
+      }
+    }
   }
 }
 
+// var menuclick = document.querySelectorAll('.button-one');
 
-// var top1 = $('#home').offset().top;
-// var top2 = $('#about').offset().top;
-// var top3 = $('#portfolio').offset().top;
+// for (var i = 0; i < menuclick.length; i++) {
+//   if (document.getElementById('navigation').classList.contains('open')) {
+//     menuclick[i].addEventListener('click', menuToggle, false);
+// }}
 
-// $(document).scroll(function() {
-//   var scrollPos = $(document).scrollTop();
-//   if (scrollPos >= top1 && scrollPos < top2) {
-//     $('#change').css('background-color', '#f00');
-//   } else if (scrollPos >= top2 && scrollPos < top3) {
-//     $('#change').css('background-color', '#0f0');
-//   } else if (scrollPos >= top3) {
-//     $('#change').css('background-color', '#00f');
-//   }
-// });
+// if (document.getElementById('navigation').classList.contains('open')&&
+// ) {
+//         menuclick[0].onclick = menuToggle
+//         menuclick[1].onclick = menuToggle
+//         menuclick[2].onclick = menuToggle
+//         menuclick[3].onclick = menuToggle
+// }
